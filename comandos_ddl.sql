c0bc31206444c42a1ea36b1fad604141b7f081b7
Tiago Pereira <tiago.pereira@triersistemas.com.br>
2022-07-05 17:27:04 -0300

    comandos ddl

---
+++ comandos_ddl.sql
@@ -0,0 +1,53 @@
+--CREATE DATABASE jovemdev_tarde;
+
+CREATE TABLE aluno (
+    aluno_id SERIAL PRIMARY KEY,
+    nome VARCHAR(200),
+    cpf VARCHAR(11) UNIQUE NOT NULL
+);
+
+CREATE TABLE turma (
+    turma_id SERIAL PRIMARY KEY ,
+    nome VARCHAR(50) NOT NULL
+);
+
+ALTER TABLE aluno ADD COLUMN turma_id INTEGER;
+
+ALTER TABLE aluno
+    ADD CONSTRAINT aluno_turma_id_fkey
+    FOREIGN KEY (turma_id)
+REFERENCES turma(turma_id);
+
+ALTER TABLE aluno ALTER COLUMN nome SET NOT NULL;
+
+ALTER TABLE aluno ADD CONSTRAINT aluno_nome_check_size
+CHECK (char_length(nome) > 5);
+
+ALTER TABLE aluno ADD COLUMN data_nascimento DATE;
+
+INSERT INTO turma (nome) VALUES ('Vespertino');
+INSERT INTO turma (nome) VALUES ('Noturno');
+select * from turma;
+
+INSERT INTO aluno (nome, cpf, turma_id, data_nascimento)
+VALUES ('Tiago João', '12345678911', 1, '1986-01-10');
+
+INSERT INTO aluno (nome, cpf, turma_id, data_nascimento)
+VALUES ('ROBERTO LUIS', '12345678910', 1, '1986-01-15');
+
+select * from aluno;
+
+----- USANDO SEQUENCIAL PARA ID
+CREATE SEQUENCE aluno_id_seq;
+CREATE TABLE aluno (
+   aluno_id INTEGER PRIMARY KEY DEFAULT nextval('aluno_id_seq'),
+   nome VARCHAR(200),
+   cpf VARCHAR(11) UNIQUE NOT NULL
+);
+
+--- USANDO UUID
+CREATE TABLE aluno (
+   aluno_id UUID PRIMARY KEY,
+   nome VARCHAR(200),
+   cpf VARCHAR(11) UNIQUE NOT NULL
+);
