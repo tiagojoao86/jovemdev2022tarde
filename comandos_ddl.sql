46cbfe18d28ea61145965674be4e79f254039da5
Tiago Pereira <tiago.pereira@triersistemas.com.br>
2022-07-07 16:33:53 -0300

    views

--- comandos_ddl.sql
+++ comandos_ddl.sql
@@ -63,3 +63,47 @@ SELECT s.staff_id,
 FROM staff s
          JOIN address a ON s.address_id = a.address_id
          JOIN city c ON a.city_id = c.city_id;
+
+         /*
+         Criar uma view sobre aluguéis (rentals), onde deverá
+         trazer as informações:
+         - Id do rental;
+         - Nome completo do cliente;
+         - Nome completo do funcionário;
+         - Valor do aluguel
+         - Nome do filme locado
+         */
+
+         CREATE OR REPLACE VIEW rentals_vw AS
+             SELECT r.rental_id,
+                    concat(c.first_name, ' ', c.last_name) as nome_cliente,
+                    concat(s.first_name, ' ', s.last_name) as nome_funcionario,
+                    f.rental_rate,
+                    f.title
+             FROM rental r
+             JOIN customer c on r.customer_id = c.customer_id
+             JOIN staff s ON r.staff_id = s.staff_id
+             JOIN inventory i on r.inventory_id = i.inventory_id
+             JOIN film f on i.film_id = f.film_id;
+
+         select * from rentals_vw;
+
+         CREATE MATERIALIZED VIEW rentals_mvw AS
+         SELECT r.rental_id,
+                concat(c.first_name, ' ', c.last_name) as nome_cliente,
+                concat(s.first_name, ' ', s.last_name) as nome_funcionario,
+                f.rental_rate,
+                f.title
+         FROM rental r
+                  JOIN customer c on r.customer_id = c.customer_id
+                  JOIN staff s ON r.staff_id = s.staff_id
+                  JOIN inventory i on r.inventory_id = i.inventory_id
+                  JOIN film f on i.film_id = f.film_id;
+
+         select * from rentals_vw where rental_id = 3;
+         select * from rentals_mvw where rental_id = 3;
+
+         refresh materialized view rentals_mvw;
+
+         select rental_rate, * from film where title = 'Graduate Lord';
+         update film set rental_rate = 10.00 where film_id = 373;
